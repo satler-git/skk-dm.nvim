8bd66392a2221110f0dd03ef3ee3ab585b5661d2
satler-git <mitsclatw24@mbr.nifty.com>
2024-08-21 18:31:01 +0900

    Add prefix config

--- lua/skk-dm.lua
+++ lua/skk-dm.lua
@@ -1,34 +1,43 @@
 local async = require("plenary.async")
 
--- @param dlist string[]
-local function setup(dlist)
-	for di in dlist do
-		if not check_downloaded(di) then
-			download(di)
+-- @param dlist { dictionaries: string[], prefix: string }
+local function setup(config)
+	local p = config.prefix
+
+	for di in config.dictionaries do
+		if not check_downloaded(di, p) then
+			download(di, p)
 		end
 	end
 end
 
+-- @param prefix string
 -- @param dict string
 -- @return boolean
-function check_downloaded(dict)
+function check_downloaded(dict, prefix)
 	-- check
 end
 
+-- @param prefix string
 -- @param dict string
-function download(dict)
+function download(dict, prefix)
 	-- dl
 end
 
+-- @param prefix string
 -- @return string[]
-local function pathes()
+local function pathes(prefix)
 	-- ユーザー向けのパス化する関数
 end
 
+-- @param prefix string
 -- @param dict string
 -- @return string
-function hashlize(dict)
+function Hashlize(dict, prefix)
 	-- 管理するファイルをurlから導く
+	-- /string$な文字列を得る
+	local file_name = string.match(dict, ".*/(.*)$")
+	return prefix .. file_name
 end
 
 return {
